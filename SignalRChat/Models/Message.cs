2343a7f2bade7ff0fa17ca6e527f554ef1c803dc
Liron Meler <lironmeler@gmail.com>
2020-09-09 12:19:28 +0300

    Add and configure the user and message models

---
+++ SignalRChat/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Models
+{
+    public class Message
+    {
+        public int MessageID { get; set; }
+        public string MessageContent { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ SignalRChat/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        
+        public ICollection<Message> Messages { get; set; }
+    }
+}
