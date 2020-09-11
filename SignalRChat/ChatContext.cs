9ffdcea52d54506372f5894953086d9ccab97900
Liron Meler <lironmeler@gmail.com>
2020-09-11 12:36:52 +0300

    Add a DB set to the context

--- SignalRChat/ChatContext.cs
+++ SignalRChat/ChatContext.cs
@@ -9,6 +9,7 @@ namespace SignalRChat
         { }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
         public DbSet<Message> Messages { get; set; }
     }
 }
