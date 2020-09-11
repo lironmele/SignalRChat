7e417f0d840df336763626915eaefd4e84943abf
Liron Meler <lironmeler@gmail.com>
2020-09-11 22:56:08 +0300

    Add a panel for the chat list

--- ChatTest/Main.Designer.cs
+++ ChatTest/Main.Designer.cs
@@ -31,11 +31,12 @@
             this.btnSendMessage = new System.Windows.Forms.Button();
             this.txtMessage = new System.Windows.Forms.TextBox();
             this.richTxtChat = new System.Windows.Forms.RichTextBox();
+            this.panelChats = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // btnSendMessage
             // 
-            this.btnSendMessage.Location = new System.Drawing.Point(547, 374);
+            this.btnSendMessage.Location = new System.Drawing.Point(631, 374);
             this.btnSendMessage.Name = "btnSendMessage";
             this.btnSendMessage.Size = new System.Drawing.Size(241, 42);
             this.btnSendMessage.TabIndex = 2;
@@ -45,31 +46,40 @@
             // 
             // txtMessage
             // 
-            this.txtMessage.Location = new System.Drawing.Point(12, 382);
+            this.txtMessage.Location = new System.Drawing.Point(282, 382);
             this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(529, 26);
+            this.txtMessage.Size = new System.Drawing.Size(343, 26);
             this.txtMessage.TabIndex = 1;
             // 
             // richTxtChat
             // 
             this.richTxtChat.BackColor = System.Drawing.SystemColors.Window;
-            this.richTxtChat.Location = new System.Drawing.Point(12, 13);
+            this.richTxtChat.Location = new System.Drawing.Point(282, 13);
             this.richTxtChat.Name = "richTxtChat";
             this.richTxtChat.ReadOnly = true;
-            this.richTxtChat.Size = new System.Drawing.Size(776, 355);
+            this.richTxtChat.Size = new System.Drawing.Size(590, 355);
             this.richTxtChat.TabIndex = 2;
             this.richTxtChat.TabStop = false;
             this.richTxtChat.Text = "";
+            // 
+            // panelChats
+            // 
+            this.panelChats.AutoScroll = true;
+            this.panelChats.Location = new System.Drawing.Point(4, 13);
+            this.panelChats.Name = "panelChats";
+            this.panelChats.Size = new System.Drawing.Size(272, 395);
+            this.panelChats.TabIndex = 3;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelChats);
             this.Controls.Add(this.richTxtChat);
             this.Controls.Add(this.txtMessage);
             this.Controls.Add(this.btnSendMessage);
             this.Name = "Main";
-            this.Size = new System.Drawing.Size(800, 428);
+            this.Size = new System.Drawing.Size(875, 428);
             this.Load += new System.EventHandler(this.Main_Load);
             this.Disposed += new System.EventHandler(this.Main_FormClosing);
             this.ResumeLayout(false);
@@ -82,6 +92,7 @@
         private System.Windows.Forms.Button btnSendMessage;
         private System.Windows.Forms.TextBox txtMessage;
         private System.Windows.Forms.RichTextBox richTxtChat;
+        private System.Windows.Forms.Panel panelChats;
     }
 }
 
