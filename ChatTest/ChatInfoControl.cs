1256b2d8a64badb2b833f9377e829a122695dbc4
Liron Meler <lironmeler@gmail.com>
2020-09-12 00:15:32 +0300

    Allow the user to load a chat by clicking its button

--- ChatTest/ChatInfoControl.cs
+++ ChatTest/ChatInfoControl.cs
@@ -6,15 +6,20 @@ namespace ChatTest
     public partial class ChatInfoControl : UserControl
     {
         public string chatName { get; set; }
-        public ChatInfoControl(string chatName)
+        private Main Parent { get; set; }
+        public ChatInfoControl(string chatName, Main Parent)
         {
             this.chatName = chatName;
+            btnOpenChat.Text = chatName;
+
+            this.Parent = Parent;
+
             InitializeComponent();
         }
 
         private void btnOpenChat_Click(object sender, EventArgs e)
         {
-
+            Parent.SelectChat(chatName);
         }
     }
 }
