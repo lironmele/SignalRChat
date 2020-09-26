028e4d8b507daf59fa9833cb3acbcdb4ee1de265
Liron Meler <lironmeler@gmail.com>
2020-09-26 16:49:15 +0300

    Add project files.

--- NoteMail/LoginActivity.cs
+++ NoteMail/LoginActivity.cs
@@ -7,7 +7,7 @@ using Android.Widget;
 namespace NoteMail
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class LoginActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
